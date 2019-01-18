eef4bd25833da6f5a86e893ea1e85a517d30800e
Vincent RISS <v.riss@pickup-services.com>
2019-01-18 10:59:55 +0100

    Prototypage de la page de vote

---
+++ CatMash.UI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatMash.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Vote()
+        {
+            return new JsonResult(new { success = true });
+        }
+    }
+}
